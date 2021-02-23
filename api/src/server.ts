8f6edca733b08d63c163d52e1fbdab615c9252e8
Fabricio V <fabriciovellone@gmail.com>
2021-02-23 20:31:55 +0100

    lesson1 finished

---
+++ api/src/server.ts
@@ -0,0 +1,18 @@
+import 'reflect-metadata';
+import express from 'express';
+import './database';
+
+const app = express();
+
+app.get("/", (request, response) => {
+  return response.json({
+    message: "Hello World - NWL04"
+  });
+});
+
+app.post("/", (request, response) => {
+
+  return response.json({message: "os dados foram salvos com sucesso"});
+})
+
+app.listen(3333,  () => console.log("Server is running!"));
